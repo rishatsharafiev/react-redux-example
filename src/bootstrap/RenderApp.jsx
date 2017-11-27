c6f8e2de252a4f181654c1a5eea93d7cf0004ffb
Rishat Sharafiev <rishatsharafiev@ya.ru>
2017-11-27 12:26:20 +0300

    Поправлен баг, когда обновление компонентов не вызывало перерендер dom

--- src/bootstrap/RenderApp.jsx
+++ src/bootstrap/RenderApp.jsx
@@ -4,23 +4,28 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from 'bootstrap/Root';
 
-let hotModuleCallbackRegistered = false;
-
-export default function renderApp(target = document.getElementById('root')) {
+const render = (Component, target = document.getElementById('root')) => {
   if (!target) {
     throw new Error('Bad target');
   }
 
   ReactDOM.render(
-    (
-      <AppContainer>
-        <Root />
-      </AppContainer>
-    ), target,
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    target,
   );
+};
 
-  if (module.hot && !hotModuleCallbackRegistered) {
-    hotModuleCallbackRegistered = true;
-    module.hot.accept('bootstrap/Root', () => { renderApp(target); });
+export default function renderApp(target) {
+  render(Root, target);
+
+  if (module.hot) {
+    module.hot.accept('bootstrap/Root', () => {
+      /* eslint-disable global-require */
+      const NextRoot = require('bootstrap/Root').default;
+      render(NextRoot, target);
+      /* eslint-enable global-require */
+    });
   }
 }
