0fc596f7fe9a33ffbbd9e5d38e24ed9396d74502
Rishat Sharafiev <rishatsharafiev@ya.ru>
2017-11-28 13:40:31 +0300

    Поправлен порядок импорта модулей

--- src/components/common/textInput.jsx
+++ src/components/common/textInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Input } from 'element-react'
 import PropTypes from 'prop-types'
+import { Input } from 'element-react'
 
 const TextInput = ({ input, type }) => (
   <div>
